bba52e56e1b00e636e0dad2bb1f93bd73912bf21
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-16 12:42:10 +0300

    initial commit

---
+++ CS/Default.aspx.cs
@@ -0,0 +1,74 @@
+﻿using DevExpress.Web;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page {
+    Dictionary<String, String> extensionsDisplayName;
+
+    Dictionary<String, String> ExtensionsDisplayName {
+        get {
+            if (extensionsDisplayName == null) {
+                extensionsDisplayName = new Dictionary<String, String>();
+                extensionsDisplayName.Add(".txt", "Text Document (.txt)");
+                extensionsDisplayName.Add(".rtf", "Rich Text Document (.rtf)");
+                extensionsDisplayName.Add(".xml", "XML File (.xml)");
+                extensionsDisplayName.Add(".png", "PNG image (.png)");
+                extensionsDisplayName.Add(".jpg", "JPEG image (.jpg)");
+                extensionsDisplayName.Add(".mp3", "MPEG Layer 3 Audio File (.mp3)");
+                extensionsDisplayName.Add(".avi", "AVI File (.avi)");
+                extensionsDisplayName.Add(".zip", "Compressed (zipped) Folder (.zip)");
+            }
+            return extensionsDisplayName;
+        }
+    }
+
+    public string GetItemName(FileManagerItem item) {
+        if (item == null)
+            return String.Empty;
+        FileManagerFile file = item as FileManagerFile;
+        if (file != null)
+            return Path.GetFileNameWithoutExtension(file.RelativeName);
+        else
+            return item.Name;
+    }
+    public string GetItemType(FileManagerItem item) {
+        if (item == null) return String.Empty;
+        FileManagerFile file = item as FileManagerFile;
+        if (file != null) {
+            if (ExtensionsDisplayName.ContainsKey(file.Extension))
+                return ExtensionsDisplayName[file.Extension];
+        }
+        else {
+            return item.Name.Equals("..") ? String.Empty : "Folder";
+        }
+        return String.Empty;
+    }
+
+    public string GetSize(FileManagerItem item) {
+        if (item == null) return String.Empty;
+        FileManagerFolder folder = item as FileManagerFolder;
+        if (folder != null) {
+            if (folder.Name.Equals(".."))
+                return String.Empty;
+            else
+                return string.Format("{0:f} Kb", CountFolderSize(folder) / 1024);
+        }
+        return item.Length / 1024 + " Kb";
+    }
+
+    private float CountFolderSize(FileManagerFolder folder, float length = 0) {
+        FileManagerFile[] files = folder.GetFiles();
+        foreach (FileManagerFile file in files)
+            length += file.Length;
+
+        FileManagerFolder[] folders = folder.GetFolders();
+        foreach (FileManagerFolder childFolder in folders)
+            length = CountFolderSize(childFolder, length);
+        return length;
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,14 @@
+# ASPxFileManager - A simple implementation of ItemTemplate for the Details View
+
+
+<p>This example demonstrates how to implement ItemTemplate of <a href="https://documentation.devexpress.com/AspNet/CustomDocument9030.aspx">ASPxFileManager</a> in Details View mode. </p>
+<br><br>
+
+
+<h3>Description</h3>
+
+<p>The main idea of this example is to&nbsp;demonstrate&nbsp;how&nbsp;ItemTemplate&nbsp;can be made in Details View of ASPxFileManager.<br>ItemTemplate was implemented in&nbsp;the FileManagerDetailsColumn tag and contains the ASPxLabel control. The text for the label is prepared in public methods stored in code behind file. <br>This approach allows you to easily operate with the&nbsp;<a href="https://documentation.devexpress.com/#AspNet/clsDevExpressWebFileManagerItemtopic">FileManagerItem</a>&nbsp;on the server side and&nbsp;retrieve data in different formats.</p>
+
+<br/>
+
+
